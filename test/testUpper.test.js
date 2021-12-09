8324a289071f8afd819cc781544169d447ba77d6
Fabian-Ojeda <fabian,ojeda01@uptc.edu.co>
2021-12-09 17:26:41 -0500

    Corrección de error

--- test/testUpper.test.js
+++ test/testUpper.test.js
@@ -2,7 +2,7 @@ const upper = require('../Functions/upper')
 
 describe('Paso a Mayusculas', () => {
     test('hola mundo debe ser HOLA MUNDO', ()=>{
-        const result = upper('esto nos dara un error')
+        const result = upper('hola mundo')
         expect(result).toBe('HOLA MUNDO')
     })
 })
